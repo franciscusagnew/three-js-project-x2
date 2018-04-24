948585a165987def6288f56668f2e0379854fcac
Franciscus Agnew <franciscusagnew@users.noreply.github.com>
2018-04-24 09:37:25 -0700

    Added ascii-render.html .\js\AsciiEffect.js .\js\CanvasRenderer.js .\js\Projector.js .\js\TrackballControls.js .\js\ascii-render.js

---
+++ js/ascii-render.js
@@ -0,0 +1,81 @@
+var container; 
+var stats;
+var camera;
+var controls;
+var scene; 
+var renderer;
+var effect;
+var sphere, plane;
+var start = Date.now();
+
+function init() {
+	var width = window.innerWidth || 2;
+	var height = window.innerHeight || 2;
+
+	var width = window.innerWidth || 2;
+	var height = window.innerHeight || 2;
+	container = document.createElement( 'div' );
+	document.body.appendChild( container );
+	var info = document.createElement( 'div' );
+	info.style.position = 'absolute';
+	info.style.top = '10px';
+	info.style.width = '100%';
+	info.style.textAlign = 'center';
+	info.innerHTML = 'Drag to change the view';
+	container.appendChild( info );
+	camera = new THREE.PerspectiveCamera( 70, width / height, 1, 1000 );
+	camera.position.y = 150;
+	camera.position.z = 500;
+	controls = new THREE.TrackballControls( camera );
+	scene = new THREE.Scene();
+	scene.background = new THREE.Color( 0xf0f0f0 );
+	var light = new THREE.PointLight( 0xffffff );
+	light.position.set( 500, 500, 500 );
+	scene.add( light );
+	var light = new THREE.PointLight( 0xffffff, 0.25 );
+	light.position.set( - 500, - 500, - 500 );
+	scene.add( light );
+	sphere = new THREE.Mesh( new THREE.SphereGeometry( 200, 20, 10 ), new THREE.MeshLambertMaterial() );
+	scene.add( sphere );
+	// Plane
+	plane = new THREE.Mesh( new THREE.PlaneBufferGeometry( 400, 400 ), new THREE.MeshBasicMaterial( { color: 0xe0e0e0 } ) );
+	plane.position.y = - 200;
+	plane.rotation.x = - Math.PI / 2;
+	scene.add( plane );
+	renderer = new THREE.CanvasRenderer();
+	renderer.setSize( width, height );
+	// container.appendChild( renderer.domElement );
+	effect = new THREE.AsciiEffect( renderer );
+	effect.setSize( width, height );
+	container.appendChild( effect.domElement );
+	stats = new Stats();
+	container.appendChild( stats.dom );
+	//
+	window.addEventListener( 'resize', onWindowResize, false );
+}
+
+function onWindowResize() {
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	effect.setSize( window.innerWidth, window.innerHeight );
+}
+
+function animate() {
+	requestAnimationFrame( animate );
+	stats.begin();
+	render();
+	stats.end();
+}
+
+function render() {
+	var timer = Date.now() - start;
+	sphere.position.y = Math.abs( Math.sin( timer * 0.002 ) ) * 150;
+	sphere.rotation.x = timer * 0.0003;
+	sphere.rotation.z = timer * 0.0002;
+	controls.update();
+	effect.render( scene, camera );
+}
+
+init();
+animate();
